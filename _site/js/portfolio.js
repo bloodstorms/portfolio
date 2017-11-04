2e12a90576d60b0346ccfe8b13ab4c6da5c6b774
Tiffany VACHEZ <tiffany.vachez@toucantoco.com>
2017-11-05 00:02:06 +0100

    base structure

---
+++ _site/js/portfolio.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  $(window).resize(function() { // Enter in the function when the window is rezised
+    $('#content').height($(window).height()); // Set the height of the window to the .content
+  });
+
+  $(window).trigger('resize'); // Add listener to resize function
+
+  $(window).scroll(function(){
+    var $scrollTop = $(window).scrollTop(),
+        $windowHeight = $(window).height();
+
+    if($scrollTop > 0) {
+        $("nav").addClass("fixed");
+    }
+    else {
+        $("nav").removeClass("fixed");
+    }
+  });
+
+	$('.js-scrollTo').on('click', function() { // On click on the anchor .js-scrollTo
+		var page = $(this).attr('href'); // Select the href
+
+		$('html, body').animate( { scrollTop: $(page).offset().top }, 600 ); // Scroll animation
+		return false;
+	});
+});
